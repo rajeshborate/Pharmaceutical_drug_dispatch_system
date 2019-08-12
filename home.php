940499233bdaf0cff18605036c7f1fa55c36348c
Rajesh Borate <rajeshborate08@gmail.com>
2019-08-12 09:25:13 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Home Page	</title>
+		<link rel="stylesheet" href="rulesignup.css"/>
+	</head>
+	<body>
+		<p id="heading"><em>Pharmaceutical Drug Dispatch System</em></p>
+		<div id="logout">
+		<button><a href="Login.php">Logout</a></button>
+		</div><br/>
+		<table>
+		<tr height="200px">
+		<td><a id="mm" href="http://localhost/PHP/mini%20project/placeorder.php">Place Order</a></td>
+		<td><a id="mm" href="http://localhost/PHP/mini%20project/ml.php">Medicines Manufactured</a></td>
+		<td><a id="mm" href="http://localhost/PHP/mini%20project/hl.php">Hospitals Registered</a></td>
+		</tr>
+		</table>
+	</body>
+	</html>
