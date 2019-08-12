75ef7e4e37c0d876c6e372104d32ff6b47e0ce31
Rajesh Borate <rajeshborate08@gmail.com>
2019-08-12 09:27:36 +0530

    Add files via upload

---
+++ placeorder.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>	Place Order</title>
+		<link rel="stylesheet" href="rulesignup.css"/>
+	</head>
+	<body>
+		<p id="heading"><em>Pharmaceutical Drug Dispatch System</em></p>
+		<div id="logout">
+		<button><a href="Login.php">Logout</a></button>
+		</div><br/>
+		<div class="order">
+		<form action="http://localhost/PHP/mini%20project/placeorder1.php" method="post">
+			Medicine Name : <input type="text" name="mname"><br/>
+			<input type="submit">
+			<input type="reset"><br/><br/>
+		</form>
+		</div>
+		
+	</body>
+	</html>  
